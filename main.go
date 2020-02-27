fb87eff5ec3ee24971496230f6bcae57a1a4f81c
Urban <urbainishimwe@gmail.com>
2020-02-27 16:05:43 +0200

    clean up

--- main.go
+++ main.go
@@ -1,64 +1,60 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
-	"strings"
 )
 
-type requestPayloadStruct struct {
-	ProxyCondition string `json:"proxy_condition"`
-}
+// type requestPayloadStruct struct {
+// 	ProxyCondition string `json:"proxy_condition"`
+// }
 
 // Log the typeform payload and redirect url
-func logRequestPayload(requestionPayload requestPayloadStruct, proxyUrl string) {
-	log.Printf("proxy_condition: %s, proxy_url: %s\n", requestionPayload.ProxyCondition, proxyUrl)
-}
+// func logRequestPayload(requestionPayload requestPayloadStruct, proxyUrl string) {
+// 	log.Printf("proxy_condition: %s, proxy_url: %s\n", requestionPayload.ProxyCondition, proxyUrl)
+// }
 
 // Get the url for a given proxy condition
-func getProxyUrl(proxyConditionRaw string) string {
-	proxyCondition := strings.ToUpper(proxyConditionRaw)
-	URL := os.Getenv(proxyCondition)
-	if URL == "" {
-		return os.Getenv("URL")
-	}
-	return URL
-}
+// func getProxyUrl(proxyConditionRaw string) string {
+// 	proxyCondition := strings.ToUpper(proxyConditionRaw)
+// 	URL := os.Getenv(proxyCondition)
+// 	if URL == "" {
+// 		return os.Getenv("URL")
+// 	}
+// 	return URL
+// }
 
 // Get a json decoder for a given requests body
-func requestBodyDecoder(request *http.Request) *json.Decoder {
-	// Read body to buffer
-	body, err := ioutil.ReadAll(request.Body)
-	if err != nil {
-		log.Printf("Error reading body: %v\n", err)
-	}
+// func requestBodyDecoder(request *http.Request) *json.Decoder {
+// 	// Read body to buffer
+// 	body, err := ioutil.ReadAll(request.Body)
+// 	if err != nil {
+// 		log.Printf("Error reading body: %v\n", err)
+// 	}
 
-	// Because go lang is a pain in the ass if you read the body then any susequent calls
-	// are unable to read the body again....
-	request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+// 	// Because go lang is a pain in the ass if you read the body then any susequent calls
+// 	// are unable to read the body again....
+// 	request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 
-	return json.NewDecoder(ioutil.NopCloser(bytes.NewBuffer(body)))
-}
+// 	return json.NewDecoder(ioutil.NopCloser(bytes.NewBuffer(body)))
+// }
 
 // Parse the requests body
-func parseRequestBody(request *http.Request) requestPayloadStruct {
-	decoder := requestBodyDecoder(request)
+// func parseRequestBody(request *http.Request) requestPayloadStruct {
+// 	decoder := requestBodyDecoder(request)
 
-	var requestPayload requestPayloadStruct
-	err := decoder.Decode(&requestPayload)
+// 	var requestPayload requestPayloadStruct
+// 	err := decoder.Decode(&requestPayload)
 
-	if err != nil {
-		log.Println("body error", err.Error())
-	}
+// 	if err != nil {
+// 		log.Println("body error", err.Error())
+// 	}
 
-	return requestPayload
-}
+// 	return requestPayload
+// }
 
 // Serve a reverse proxy for a given url
 func serveReverseProxy(target string, res http.ResponseWriter, req *http.Request) {
@@ -69,7 +65,6 @@ func serveReverseProxy(target string, res http.ResponseWriter, req *http.Request
 	proxy := httputil.NewSingleHostReverseProxy(url)
 
 	// Update the headers to allow for SSL redirection
-	log.Println(url.Host, url.Scheme)
 	req.URL.Host = url.Host
 	req.URL.Scheme = url.Scheme
 	req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
@@ -81,11 +76,12 @@ func serveReverseProxy(target string, res http.ResponseWriter, req *http.Request
 
 // Given a request send it to the appropriate url
 func handleRequestAndRedirect(res http.ResponseWriter, req *http.Request) {
-	requestPayload := parseRequestBody(req)
-	log.Println("proxy", requestPayload.ProxyCondition)
-	url := getProxyUrl(requestPayload.ProxyCondition)
+	// requestPayload := parseRequestBody(req)
+	// log.Println("proxy", requestPayload.ProxyCondition)
+	log.Println("from ", req.RemoteAddr)
+	url := os.Getenv("URL")
 
-	logRequestPayload(requestPayload, url)
+	// logRequestPayload(requestPayload, url)
 
 	serveReverseProxy(url, res, req)
 }
@@ -110,7 +106,6 @@ func logSetup() {
 }
 
 func main() {
-	// Log setup values
 	logSetup()
 
 	// start server
